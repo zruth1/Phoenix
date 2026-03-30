1cd6f67b00164ec3baf10fa19a59631195f6aef8
Zoie Mulkey <zoiemulkey@Mac.attlocal.net>
2026-03-29 20:14:21 -0700

    add money muse page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Auth from './Pages/Auth';
 import Dashboard from './Pages/Dashboard';
+import MoneyMuse from './Pages/MoneyMuse';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Auth />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/money-muse" element={<MoneyMuse />} />
         <Route path="/" element={<Navigate to="/dashboard" />} />
       </Routes>
     </BrowserRouter>
